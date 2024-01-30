c8d8bfd0cf2234e10403ce2d9efab918a929bff7
Beau McCartney <mccartney.beausl@gmail.com>
2024-01-29 19:41:35 -0700

    choose which page to render based on a string

--- client/src/App.js
+++ client/src/App.js
@@ -5,15 +5,27 @@ import { Home } from './components/home/Home';
 import { Step1 } from './components/calculator/Step1';
 import './App.css'; // Your App specific styles
 
+function mapPageToComponent(page) {
+  switch(page) {
+    case 'step1':
+      return <Step1 />;
+    case 'home':
+      return <Home />;
+  }
+}
+
 function App() {
   const username = "Liam";
+
+  // usestate for current page
+  const [currentPage, setCurrentPage] = React.useState('step1');
 
   return (
     <div className="app-container">
       <Sidebar />
       <div className="main-content">
         <Header username={username} />
-        <Step1 />
+        {mapPageToComponent(currentPage)}
       </div>
     </div>
   );
