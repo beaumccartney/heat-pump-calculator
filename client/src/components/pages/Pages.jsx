6ca6c4972886a16dbc4ba0cfe01182259788b6c3
sid-menon <86627011+sid-menon@users.noreply.github.com>
2024-01-31 14:01:05 -0700

    Update Pages.jsx

--- client/src/components/pages/Pages.jsx
+++ client/src/components/pages/Pages.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 
 import { Home } from "../home/Home"
+import CustomComponents from "../common/CustomComponents"
 
 export const Pages = () => {
     return (
@@ -9,6 +10,7 @@ export const Pages = () => {
             <Router>
                 <Switch>
                     <Route exact path="/"><Home /></Route>
+                    <Route path="/results"><CustomComponents /></Route>
                     {/* All the routes go here */}
                 </Switch>
             </Router>
