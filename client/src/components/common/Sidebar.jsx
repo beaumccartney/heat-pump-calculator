be0ae0e62c44ada8d22edc9514a7203520d7d59b
Taaha Shakeel <81483265+taahashakeel@users.noreply.github.com>
2024-02-05 19:09:28 -0700

    Add files via upload

---
+++ client/src/components/common/Sidebar.jsx
@@ -0,0 +1,62 @@
+import React, {useState} from 'react';
+import './Sidebar.css'; // Make sure to create a corresponding CSS file
+
+// Assuming you have SVG icons or icon components ready to use
+import MenuIcon from '../assets/images/Dashboard-I.png';
+import MenuIconClicked from '../assets/images/DashboardDark-I.png';
+import SearchIcon from '../assets/images/SearchDark-I.png';
+import SearchIconClicked from '../assets/images/Search-I.png';
+import SettingsIcon from '../assets/images/Settings-I.png';
+import SettingsIconClicked from '../assets/images/SettingsWhite-I.jpg';
+import ChatIcon from '../assets/images/Chat-I.png';
+import ChatIconClicked from '../assets/images/ChatWhite-I.jpg';
+
+function Sidebar() {
+
+  const [clicked, setClicked] = useState(null);
+
+  const handleClick = (item) => {
+    setClicked(item);
+  };
+
+
+  return (
+    <div className="sidebar">
+      <div className={`sidebar-item ${clicked === 'menu' ? 'clicked' : ''}`} onClick={() => handleClick('menu')}>
+        <a href='/energy-saving-tips'>
+        <img src={clicked === 'menu' ? MenuIconClicked : MenuIcon} alt="Menu" />
+        </a>
+      </div>
+      <div className={`sidebar-item ${clicked === 'search' ? 'clicked' : ''}`} onClick={() => handleClick('search')}>
+        <a href='/intro'>
+        <img src={clicked === 'search' ? SearchIconClicked : SearchIcon} alt="Search" />
+        </a>
+      </div>
+      <div>
+        <a href='/info'> 
+      <img src={ChatIcon} alt="Chat" />
+      </a>
+      </div>
+
+      <div className="sidebar-spacer"></div>
+      <div> 
+        <a href='/settings'>
+     <img src={SettingsIcon} alt="Settings" />
+     </a>
+      </div>
+
+      {/*The below code is to be used when we have the icons for white version of these*/}
+
+      
+      {/* <div className={`sidebar-item ${clicked === 'chat' ? 'clicked' : ''}`} onClick={() => handleClick('chat')}>
+        <img src={clicked === 'chat' ? ChatIconClicked : ChatIcon} alt="Chat" />
+      </div>
+      <div className="sidebar-spacer"></div>
+      <div className={`sidebar-item ${clicked === 'settings' ? 'clicked' : ''}`} onClick={() => handleClick('settings')}>
+        <img src={clicked === 'settings' ? SettingsIconClicked : SettingsIcon} alt="Settings" />
+      </div> */}
+    </div>
+  );
+}
+
+export default Sidebar;
