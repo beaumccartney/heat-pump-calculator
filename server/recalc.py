1d86f117ecc26bfbe3a771195a83e19041fc5997
Beau McCartney <mccartney.beausl@gmail.com>
2024-02-19 13:08:17 -0800

    more explicit var names in recalc script

--- server/recalc.py
+++ server/recalc.py
@@ -1,18 +1,18 @@
 import sys
 import json
-import xlwings as xw
+import xlwings
 
 def recalc():
-    wb = xw.Book('data/ASHP Calculator - U of C.xlsm')
+    excelsheet_handle = xlwings.Book('data/ASHP Calculator - U of C.xlsm')
 
-    input_sheet  = wb.sheets['User Inputs']
+    input_sheet = excelsheet_handle.sheets['User Inputs']
 
     # NOTE: first argument must a json containing the inputs
     inputs = json.loads(sys.argv[1])
     for cell, input in zip([ 'G2', 'G4', 'G5', 'G6', ], inputs):
         input_sheet[cell].value = input
 
-    wb.app.calculate()
+    excelsheet_handle.app.calculate()
 
-    output_sheet = wb.sheets['Outputs']
+    output_sheet = excelsheet_handle.sheets['Outputs']
     return output_sheet
