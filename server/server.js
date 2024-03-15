4bac410e7225b2416914d252d76590d5f92e34c8
Beau McCartney <mccartney.beausl@gmail.com>
2024-03-15 16:11:44 -0600

    server: use cors in express router
    lets me call the api from the browser on localhost

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 // Set up routes
 const appRoutes = require('./routes/appRoutes');
