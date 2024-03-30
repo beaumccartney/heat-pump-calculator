092110827d61ccf1a33ad7ab30658413a7dbd6ff
sid-menon <86627011+sid-menon@users.noreply.github.com>
2024-03-30 15:57:49 -0600

    Update StepPage.jsx
    - Added the api calls for the heatpump data
    - Need to change the year inputs so that all of them fits
    - Need to make sure the bars change with each heatpump data value

--- client/src/components/calculator/StepPage.jsx
+++ client/src/components/calculator/StepPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { StepSidebar, SelectionForm, OptionCard, NextStep } from "./StepComponents"; // Adjust the import path as needed
 import "../../style/main.scss";
 import "./Calculator.css";
@@ -10,8 +10,12 @@ import vectorIcon from '../assets/images/dailyusage.png';
 import decisionIcon from '../assets/images/faq.png';
 import { jsPDF } from "jspdf";
 import html2canvas from 'html2canvas';
+import axios from 'axios';
+import Papa from 'papaparse';
+
 
 export const StepPage = () => {
+    const [apiData, setApiData] = useState(null);
     const [currentStep, setCurrentStep] = useState(1);
     const [selectedHomeType, setSelectedHomeType] = useState(null);
     const [submitted, setSubmitted] = useState(false);
@@ -20,6 +24,22 @@ export const StepPage = () => {
     const [selectedEfficiency, setSelectedEfficiency] = useState(null);
     const [customEfficiency, setCustomEfficiency] = useState('');
     const [efficiencySubmitted, setEfficiencySubmitted] = useState(false);
+
+    //Saved values for the all the heatpumps
+    const [h1_1, setH1_1Data] = useState(null);
+    const [h1_2, setH1_2Data] = useState(null);
+    const [h1_3, setH1_3Data] = useState(null);
+    const [h1_4, setH1_4Data] = useState(null);
+
+    const [h2_1, setH2_1Data] = useState(null);
+    const [h2_2, setH2_2Data] = useState(null);
+    const [h2_3, setH2_3Data] = useState(null);
+    const [h2_4, setH2_4Data] = useState(null);
+
+    const [h3_1, setH3_1Data] = useState(null);
+    const [h3_2, setH3_2Data] = useState(null);
+    const [h3_3, setH3_3Data] = useState(null);
+    const [h3_4, setH3_4Data] = useState(null);
 
 
 
@@ -66,10 +86,10 @@ export const StepPage = () => {
                 id: 'card1',
               title: 'HEAT PUMP #1',
               dataPoints: [
-                { label: 'UPFRONT INVESTMENT', value: '$7,500', valueWidth: '70%' }, //values will be passed in from backend
-                { label: 'COST SAVINGS', value: '$2,293', valueWidth: '30%' },
-                { label: 'ENERGY USE (GJ/YEAR)', value: '75', valueWidth: '50%' },
-                { label: 'GHG EMISSIONS (TONNES CO2E/YEAR)', value: '4.1', valueWidth: '40%' },
+                { label: 'UPFRONT INVESTMENT', value: h1_1, valueWidth: '70%' }, //values will be passed in from backend
+                { label: 'COST SAVINGS', value: h1_2, valueWidth: '30%' },
+                { label: 'ENERGY USE (GJ/YEAR)', value: h1_3, valueWidth: '50%' },
+                { label: 'GHG EMISSIONS (TONNES CO2E/YEAR)', value: h1_4, valueWidth: '40%' },
                 // Add more data points as needed
               ],
             },
@@ -77,10 +97,10 @@ export const StepPage = () => {
                 id: 'card2',
                 title: 'HEAT PUMP #2',
                 dataPoints: [
-                  { label: 'UPFRONT INVESTMENT', value: '$7,500', valueWidth: '70%' },
-                  { label: 'COST SAVINGS', value: '$2,293', valueWidth: '30%' },
-                  { label: 'ENERGY USE (GJ/YEAR)', value: '75', valueWidth: '50%' },
-                  { label: 'GHG EMISSIONS (TONNES CO2E/YEAR)', value: '4.1', valueWidth: '40%' },
+                  { label: 'UPFRONT INVESTMENT', value: h2_1, valueWidth: '70%' },
+                  { label: 'COST SAVINGS', value: h2_2, valueWidth: '30%' },
+                  { label: 'ENERGY USE (GJ/YEAR)', value: h2_3, valueWidth: '50%' },
+                  { label: 'GHG EMISSIONS (TONNES CO2E/YEAR)', value: h2_4, valueWidth: '40%' },
                   // Add more data points as needed
                 ],
               },
@@ -88,10 +108,10 @@ export const StepPage = () => {
                 id: 'card3',
                 title: 'HEAT PUMP #3',
                 dataPoints: [
-                  { label: 'UPFRONT INVESTMENT', value: '$7,500', valueWidth: '70%' },
-                  { label: 'COST SAVINGS', value: '$2,293', valueWidth: '30%' },
-                  { label: 'ENERGY USE (GJ/YEAR)', value: '75', valueWidth: '50%' },
-                  { label: 'GHG EMISSIONS (TONNES CO2E/YEAR)', value: '4.1', valueWidth: '40%' },
+                  { label: 'UPFRONT INVESTMENT', value: h3_1, valueWidth: '70%' },
+                  { label: 'COST SAVINGS', value: h3_2, valueWidth: '30%' },
+                  { label: 'ENERGY USE (GJ/YEAR)', value: h3_3, valueWidth: '50%' },
+                  { label: 'GHG EMISSIONS (TONNES CO2E/YEAR)', value: h3_4, valueWidth: '40%' },
                   // Add more data points as needed
                 ],
               },
@@ -102,6 +122,7 @@ export const StepPage = () => {
 
     // Function to handle the selection of an efficiency percentage
     const handleEfficiencySelect = (efficiency) => {
+        console.log(efficiency);
         setSelectedEfficiency(efficiency);
         // Potentially move to the next step or handle the efficiency selection
         setCurrentStep(currentStep + 1);
@@ -129,6 +150,7 @@ export const StepPage = () => {
     };
 
     const handleHomeYearSelect = (yearRange) => {
+        console.log(yearRange);
         setSelectedHomeYear(yearRange);
         // Potentially move to the next step or handle the year range selection
         setCurrentStep(currentStep + 1);
@@ -137,6 +159,7 @@ export const StepPage = () => {
 
     // Function to handle the selection of a home type
     const handleHomeTypeSelect = (type) => {
+        console.log(type);
         setSelectedHomeType(type);
         // Set the next step or navigate to a new page/component
         // For example, incrementing the current step:
@@ -144,6 +167,70 @@ export const StepPage = () => {
         setCurrentStep(currentStep + 1);
     };
 
+    const parseHomeSize = (homeType) => {
+        const sizePattern = /\((\d+) - (\d+) SQFT\)/;
+        const match = homeType.match(sizePattern);
+        if (match) {
+            const size1 = parseInt(match[1], 10);
+            const size2 = parseInt(match[2], 10);
+            return (size1 + size2) / 2; // Returns the average size
+        }
+        return 0; // Return a default or error value if parsing fails
+    };
+    
+    useEffect(() => {
+        // Check if the necessary conditions are met before fetching API data
+        const hasNecessaryData = selectedHomeYear && ((selectedHomeType && !customHomeSize) || (!selectedHomeType && customHomeSize)) && (selectedEfficiency || (customEfficiency && efficiencySubmitted));
+        console.log(`Ready for API call: ${hasNecessaryData}`); // Troubleshooting print statement
+    
+        if (hasNecessaryData) {
+            console.log("Preparing to fetch API data...");
+            fetchApiData();
+        }
+    }, [selectedHomeYear, customHomeSize, selectedEfficiency, customEfficiency, efficiencySubmitted]);
+    
+    const fetchApiData = async () => {
+        try {
+            const homeSizeAverage = selectedHomeType ? parseHomeSize(selectedHomeType) : parseInt(customHomeSize, 10);
+            const inputs = { buildYear: selectedHomeYear, sizeOfHome: homeSizeAverage };
+            console.log("API Inputs:", inputs); // Print the inputs for troubleshooting
+    
+            // Uncomment and use your actual API endpoint and request method
+            const response = await axios.post('http://localhost:3001/api/calc', inputs)
+            console.log("API Response:", response.data); // Log the API response for debugging
+            const parsed = Papa.parse(response.data);
+
+
+            //Takes from the values and sets them from the first heatpump
+            //First index value is to choose the heatpump, second is to choose the values from the heatpump
+            setH1_1Data('$ '+Math.round(parsed.data[4][1]));
+            setH1_2Data('$ '+Math.round(parsed.data[4][4]));
+            setH1_3Data(Math.round(parsed.data[4][5]));
+            setH1_4Data(parseFloat(parsed.data[4][6]).toFixed(1));
+
+            //Takes the values and sets them from the second heat pump
+            setH2_1Data('$ '+Math.round(parsed.data[6][1]));
+            setH2_2Data('$ '+Math.round(parsed.data[6][4]));
+            setH2_3Data(Math.round(parsed.data[6][5]));
+            setH2_4Data(parseFloat(parsed.data[6][6]).toFixed(1));
+
+            //Takes the values and sets the from the 4th heatpump
+            setH3_1Data('$ '+Math.round(parsed.data[10][1]));
+            setH3_2Data('$ '+Math.round(parsed.data[10][4]));
+            setH3_3Data(Math.round(parsed.data[10][5]));
+            setH3_4Data(parseFloat(parsed.data[10][6]).toFixed(1));
+
+            console.log(parsed.data[4][1]);
+            console.log(parsed.data[4][4]);
+            console.log(parsed.data[4][5]);
+            console.log(parsed.data[4][6]);
+    
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+    
+    
     if (selectedEfficiency || (customEfficiency && efficiencySubmitted === true)) {
         return (
             <div>
@@ -315,25 +402,36 @@ export const StepPage = () => {
                         </div>
                 </div>
                 <div className="content">
-                    <div className="options">
-                        {/* Replace the previous options with year range options */}
-                        <div className="option-year" onClick={() => handleHomeYearSelect("1940-1970")}>
-                            <div className="label-year">1940-1970</div>
+                    <div className="options"> {/* First group of four */}
+                        {[
+                        "<1949",
+                        "1950-1959",
+                        "1960-1981",
+                        "1982-1990"
+                        ].map((range) => (
+                        <div key={range} className="option-year" onClick={() => handleHomeYearSelect(range)}>
+                            <div className="label-year">{range}</div>
                         </div>
-                        <div className="option-year" onClick={() => handleHomeYearSelect("1970-2000")}>
-                            <div className="label-year">1970-2000</div>
-                        </div>
-                        <div className="option-year" onClick={() => handleHomeYearSelect("2000-2024")}>
-                            <div className="label-year">2000-2024</div>
-                        </div>
+                        ))}
                     </div>
-                    {/* Remove the custom size input as it's not part of this step */}
+                    <div className="options"> {/* Second group of four */}
+                        {[
+                        "1991-1997",
+                        "1998-2006",
+                        "2007-2014",
+                        "2015-present"
+                        ].map((range) => (
+                        <div key={range} className="option-year" onClick={() => handleHomeYearSelect(range)}>
+                            <div className="label-year">{range}</div>
+                        </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
         );
     }
-
+    
     return (
         <div className="home-type-selection">
             <div className="title">
@@ -395,3 +493,4 @@ export const StepPage = () => {
 
     );
 };
+
